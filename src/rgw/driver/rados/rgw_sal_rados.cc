549034c8bc163552eb2ea2af24efecd002a86af8
Shilpa Jagannath <smanjara@redhat.com>
2024-01-18 13:47:49 -0500

    rgw/multisite: add a flag to bilog_flags and use it to set/unset null version
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>
    (cherry picked from commit 485020f97b08222f696d7aaf95f9ec3b49c5d171)
    resolves rhbz#1967576

--- src/rgw/driver/rados/rgw_sal_rados.cc
+++ src/rgw/driver/rados/rgw_sal_rados.cc
@@ -3410,6 +3410,7 @@ int RadosObject::RadosDeleteOp::delete_obj(const DoutPrefixProvider* dpp, option
   if (params.objv_tracker) {
       parent_op.params.check_objv = params.objv_tracker->version_for_check();
   }
+  parent_op.params.null_verid = params.null_verid;
 
   int ret = parent_op.delete_obj(y, dpp, flags & FLAG_LOG_OP);
   if (ret < 0)
