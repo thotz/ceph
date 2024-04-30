d383977ccbaaa560a1d4e72307c447eb6440a672
Mark Kogan <mkogan@redhat.com>
2024-04-30 11:20:54 +0000

    rgw/beast: fix crash observed in SSL stream.async_shutdown()
    Fixes: https://tracker.ceph.com/issues/65664
    Resolves: rhbz#2331652
    
    Signed-off-by: Mark Kogan <mkogan@redhat.com>
    (cherry picked from commit 1815b0c358ca2b3940675c16fd40f9ca20c63a3f)

--- src/rgw/rgw_asio_frontend.cc
+++ src/rgw/rgw_asio_frontend.cc
@@ -1064,10 +1064,12 @@ void AsioFrontend::accept(Listener& l, boost::system::error_code ec)
         handle_connection(context, env, stream, timeout, header_limit,
                           conn->buffer, true, pause_mutex, scheduler.get(),
                           uri_prefix, ec, yield);
-        if (!ec) {
+
+        if (!ec || ec == http::error::end_of_stream) {
           // ssl shutdown (ignoring errors)
           stream.async_shutdown(yield[ec]);
         }
+
         conn->socket.shutdown(tcp::socket::shutdown_both, ec);
       }, make_stack_allocator());
   } else {
