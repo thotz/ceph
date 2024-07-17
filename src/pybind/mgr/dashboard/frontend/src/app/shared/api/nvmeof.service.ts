3beeade8f98badca85ed4fb63d451c6c2ffe6dda
Afreen Misbah <afreen23.git@gmail.com>
2024-07-17 15:46:23 +0530

    mgr/dashboard: Create and delete listeners
    Fixes https://tracker.ceph.com/issues/66996
    
    - list listener under subsystems
    - delete listener
    - create listener
    
    Signed-off-by: Afreen Misbah <afreen23.git@gmail.com>
    (cherry picked from commit a4f2eefe21f973d532a7e08d1ce4e977f39dfe88)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table/table.component.ts
    	src/pybind/mgr/dashboard/frontend/src/app/shared/enum/cell-template.enum.ts
    
    Resolves: rhbz#2298883
    (cherry picked from commit cde6e1c4c2aef097c5c1f2149f9181f580a1d647)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/api/nvmeof.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/api/nvmeof.service.ts
@@ -5,9 +5,6 @@ import _ from 'lodash';
 import { Observable, of as observableOf } from 'rxjs';
 import { catchError, mapTo } from 'rxjs/operators';
 
-import _ from 'lodash';
-import { Observable, of as observableOf } from 'rxjs';
-import { catchError, mapTo } from 'rxjs/operators';
 
 export const MAX_NAMESPACE = 1024;
 
@@ -69,6 +66,29 @@ export class NvmeofService {
         e?.preventDefault();
         return observableOf(false);
       })
+    );
+  }
+
+  // listeners
+  listListeners(subsystemNQN: string) {
+    return this.http.get(`${BASE_URL}/subsystem/${subsystemNQN}/listener`);
+  }
+
+  createListener(subsystemNQN: string, request: ListenerRequest) {
+    return this.http.post(`${BASE_URL}/subsystem/${subsystemNQN}/listener`, request, {
+      observe: 'response'
+    });
+  }
+
+  deleteListener(subsystemNQN: string, hostName: string, traddr: string, trsvcid: string) {
+    return this.http.delete(
+      `${BASE_URL}/subsystem/${subsystemNQN}/listener/${hostName}/${traddr}`,
+      {
+        observe: 'response',
+        params: {
+          trsvcid
+        }
+      }
     );
   }
 }
