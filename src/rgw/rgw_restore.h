a39db64bc2da5735e14f91046a2e6fd565b339e4
Jiffin Tony Thottan <thottanjiffin@gmail.com>
2025-10-09 19:13:52 +0530

    rgw/adminops: support for adding restore operation
    Adding support for restore cli in admin ops, added existing clis list and statusw
    
    Fixes: https://tracker.ceph.com/issues/70931
    
    Signed-off-by: Jiffin Tony Thottan <thottanjiffin@gmail.com>

--- src/rgw/rgw_restore.h
+++ src/rgw/rgw_restore.h
@@ -66,6 +66,17 @@ struct RestoreEntry {
 };
 WRITE_CLASS_ENCODER(RestoreEntry)
 
+/// list restore status of objects in the bucket
+int list(const DoutPrefixProvider* dpp, rgw::sal::Driver* driver,
+        RestoreEntry& entry, std::optional<std::string> restore_status_filter,
+        std::string& err_msg, RGWFormatterFlusher& flusher, optional_yield y);
+
+/// restore status of an object in a bucket
+int status(const DoutPrefixProvider* dpp, rgw::sal::Driver* driver,
+           RestoreEntry& entry, std::string& err_msg,
+           RGWFormatterFlusher& flusher, optional_yield y);
+
+
 class Restore : public DoutPrefixProvider {
   CephContext *cct;
   rgw::sal::Driver* driver;
@@ -165,6 +176,15 @@ public:
                               const std::string& version_id,
                               const rgw::notify::EventTypeList& event_types,
                               optional_yield y);
+  // list restore status of objects in the bucket
+  int list(const DoutPrefixProvider* dpp, RestoreEntry& entry,
+           std::optional<std::string> restore_status_filter, std::string& err_msg,
+           RGWFormatterFlusher& flusher, optional_yield y);
+
+  // restore status of an object in a bucket
+  int status(const DoutPrefixProvider* dpp, RestoreEntry& entry,
+             std::string& err_msg, RGWFormatterFlusher& flusher,
+             optional_yield y);
 };
 
 } // namespace rgw::restore
